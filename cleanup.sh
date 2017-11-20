dbfaa0570c349408424c981b92cff0453de30ac1
Josiah Schwab <jschwab@gmail.com>
2017-11-20 15:55:08 -0800

    For now, don't do cleanup
    Queue dependencies aren't being honored

--- cleanup.sh
+++ cleanup.sh
@@ -34,4 +34,4 @@ grep "fail" binary.log >> output.txt
 mail -s "MESA Test Suite r${VERSION}" -a star.log -a binary.log -a install.log ${MY_EMAIL_ADDRESS} < output.txt
 
 # clean stuff up
-rm install.log star.log-* star.log binary.log-* binary.log
+# rm install.log star.log-* star.log binary.log-* binary.log

17145b008d04bf3d9af8fa04dd849ac2bbe253c1
Josiah Schwab <jschwab@gmail.com>
2017-11-21 11:21:49 -0800

    Go to each_test_run equivalent

--- binary.sh
+++ binary.sh
@@ -10,7 +10,7 @@ module load mesasdk/${MESASDK_VERSION}
 clean_caches
 
 if [ -n "${USE_MESA_TEST}" ]; then
-    mesa_test test_one ${MESA_DIR} ${PBS_ARRAYID} --module=binary --force --no-submit
+    mesa_test test_one ${MESA_DIR} ${PBS_ARRAYID} --module=binary --force --no-submit --no-diff
 else
     cd ${MESA_DIR}/binary/test_suite
     ./${MESA_TEST_COMMAND} ${PBS_ARRAYID}

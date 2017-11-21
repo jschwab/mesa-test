8fc8deb3492907075320ad9560cf31f6029a9917
Josiah Schwab <jschwab@gmail.com>
2017-11-20 16:38:30 -0800

    Fix typo

--- binary.sh
+++ binary.sh
@@ -9,7 +9,7 @@
 module load mesasdk/${MESASDK_VERSION}
 clean_caches
 
-if [ -n USE_MESA_TEST ]; then
+if [ -n ${USE_MESA_TEST} ]; then
     mesa_test test_one ${MESA_DIR} ${PBS_ARRAYID} --module=binary --force
 else
     cd ${MESA_DIR}/binary/test_suite

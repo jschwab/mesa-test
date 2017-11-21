1379652cb3191ba494f577881691009e822aab69
Josiah Schwab <jschwab@gmail.com>
2017-11-20 17:02:50 -0800

    Fix missing quotes

--- star.sh
+++ star.sh
@@ -9,7 +9,7 @@
 module load mesasdk/${MESASDK_VERSION}
 clean_caches
 
-if [ -n ${USE_MESA_TEST} ]; then
+if [ -n "${USE_MESA_TEST}" ]; then
     mesa_test test_one ${MESA_DIR} ${PBS_ARRAYID} --module=star --force
 else
     cd ${MESA_DIR}/star/test_suite

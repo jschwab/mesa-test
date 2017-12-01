fcf931d54d96b93673bf2403ceb79ecfc8149f20
Josiah Schwab <jschwab@gmail.com>
2017-12-01 15:47:17 -0800

    Multiple dependencies empirically don't work on hyades

--- test-mesa.sh
+++ test-mesa.sh
@@ -102,7 +102,7 @@ cd ${MESA_DIR}/binary/test_suite
 export NTESTS=$(./count_tests)
 cd -
 
-BINARY_JOBID=$(qsub binary.sh -o ${MESA_DIR}/binary.log -W depend=afterok:${INSTALL_JOBID} -t 1-${NTESTS})
+BINARY_JOBID=$(qsub binary.sh -o ${MESA_DIR}/binary.log -W depend=afterok:${STAR_JOBID} -t 1-${NTESTS})
 
 # send the email
-qsub cleanup.sh -W depend=afterokarray:${STAR_JOBID}:${BINARY_JOBID}
+qsub cleanup.sh -W depend=afterokarray:${BINARY_JOBID}
